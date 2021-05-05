6a2176db089ebe66b7513b831f63ae0a71f76e6a
max-b10 <bungaymax@gmail.com>
2021-05-05 10:28:22 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("testing");
+console.log("app.js running");
+
+const speechSynthesis = window.speechSynthesis;
+const textArea = document.querySelector("#enterText");
+const speakButton = document.querySelector("#speakButton");
+
+speakButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  speakText(textArea.value);
+});
+
+function speakText(text) {
+  const speech = new SpeechSynthesisUtterance(text);
+  speechSynthesis.speak(speech);
+}
