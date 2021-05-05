73253e17366a3fc97d55c88f3996b3e4b3c338d1
max-b10 <bungaymax@gmail.com>
2021-05-05 21:54:37 +0100

    updates

--- app.js
+++ app.js
@@ -1,15 +1,55 @@
 console.log("app.js running");
 
 const speechSynthesis = window.speechSynthesis;
+const form = document.querySelector("#form");
 const textArea = document.querySelector("#enterText");
 const speakButton = document.querySelector("#speakButton");
+const chooseVoice = document.querySelector("#choose-voice");
 
-speakButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  speakText(textArea.value);
-});
+let allVoices = [];
+
+// Populate the voice selection dropdown:
+const populateVoices = () => {
+  allVoices = speechSynthesis.getVoices();
+  allVoices.forEach((voice) => {
+    // Create option element for each voice type:
+    const option = document.createElement("option");
+    // Fill option with voice and language
+    option.textContent = `${voice.name} (${voice.lang})`;
+
+    option.setAttribute("data-lang", voice.lang);
+    option.setAttribute("data-name", voice.name);
+    chooseVoice.appendChild(option);
+  });
+};
+populateVoices();
+
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = populateVoices;
+}
 
 function speakText(text) {
-  const speech = new SpeechSynthesisUtterance(text);
-  speechSynthesis.speak(speech);
+  const speechText = new SpeechSynthesisUtterance(textArea.value);
+
+  // Select the voice from the options dropdown:
+  const selectedVoice = chooseVoice.selectedOptions[0].getAttribute(
+    "data-name"
+  );
+
+  allVoices.forEach((voice) => {
+    if (voice.name === selectedVoice) {
+      speechText.voice = voice;
+    }
+  });
+  console.log(speechText);
+  speechText.volume = 1;
+  speechText.rate = 1;
+  speechText.pitch = 1;
+  speechSynthesis.speak(speechText);
 }
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  speakText();
+});
+chooseVoice.addEventListener("change", () => speakText());
